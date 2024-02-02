47110aa663f017b13b287cdc9c913b39342c6480
albindavidc <albindavidc007@gmail.com>
2024-02-02 13:39:29 +0530

    Second commit

--- index1.js
+++ index1.js
@@ -1,21 +1,12 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
+  res.send("Hello");
 });
 
 app.listen(port, () => {
--- index2.js
+++ index2.js
@@ -1,17 +1,19 @@
 import express from "express";
-import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
-app.use(morgan("tiny"));
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();
+}
 
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
